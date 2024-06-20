19a5150d119bb2be9abda64b09b6c2dc69195727
mimmuv <mmuvoti@gmail.com>
2024-06-20 13:58:41 +0200

    formatted temperature and icon for the weekly forecast

--- src/Weeklyforecast.js
+++ src/Weeklyforecast.js
@@ -1,32 +1,51 @@
 import React from "react";
 import axios from "axios";
 import "./Weeklyforecast.css";
+import { useState } from "react";
 
 export default function Weeklyforecast(props) {
   //console.log(props);
+  let [ready, setReady] = useState(false);
+  let [forecastweather, setWeather] = useState(null);
 
   // function getWeeklyData() {
-  let lat = props.coordinates.lat;
-  let lon = props.coordinates.lon;
-  let apiKey = "1ee4264117b73d2263eecd562f31ef5c";
-  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiURL).then(setWeeklyForecast);
+
   // }
 
   function setWeeklyForecast(response) {
     console.log(response);
+    setReady(true);
+    setWeather(response.data.daily);
   }
-  return (
-    <div className="WeeklyForecast">
-      <div className="row">
-        <div className="col">
-          <div className="forecastDay">Mon</div>
-          <div>⛅️</div>
-          <span className="maxTemp">25º</span>
-          &nbsp;
-          <span className="minTemp">5º</span>
+  if (ready) {
+    return (
+      <div className="WeeklyForecast">
+        <div className="row">
+          <div className="col">
+            <div className="forecastDay">{forecastweather[0].dt}</div>
+            <div>
+              <img
+                className="weeklyicon"
+                src={`https://openweathermap.org/img/wn/${forecastweather[0].weather[0].icon}@2x.png`}
+                alt="weeklyweathericon"
+              />
+            </div>
+            <span className="maxTemp">
+              {Math.round(forecastweather[0].temp.max)}º
+            </span>
+            &nbsp;
+            <span className="minTemp">
+              {Math.round(forecastweather[0].temp.min)}º
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let lat = props.coordinates.lat;
+    let lon = props.coordinates.lon;
+    let apiKey = "1ee4264117b73d2263eecd562f31ef5c";
+    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiURL).then(setWeeklyForecast);
+  }
 }
